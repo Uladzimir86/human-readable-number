477d614f8a4dc4d2681594d8c8e87bcebb369b2d
Uladzimir86 <olodja@rambler.ru>
2021-02-23 17:12:06 +0300

    feat: add solution to src/index.js"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function toReadable (number) {
-  
+
+    let arrEd = ['zero',  'one',  'two',  'three',  'four',
+      'five', 'six',  'seven',  'eight',  'nine', 'ten',
+      'eleven', 'twelve','thirteen','fourteen', 'fifteen', 'sixteen',
+       'seventeen', 'eighteen', 'nineteen', 'twenty'];
+     let arrDes = ['0','1','twenty', 'thirty', 'forty', 'fifty', 'sixty', 
+     'seventy', 'eighty', 'ninety'];
+
+    if (number <= 21) return arrEd[number];
+
+    let arrNumber = String(number).split('').reverse();
+
+    sot = function(x) {
+        if (x == null) return '';
+        return arrEd[+x]+' '+'hundred';
+    }
+    des = function(y) {
+      if (y == "0") return '';
+      if (y == '1') return arrEd[+('1'+arrNumber[0])];
+        return arrDes[+y];
+    }
+    ed = function(z) {
+        if (z == "0") return '';
+        return arrEd[+z];
+    }
+
+
+    if(arrNumber.length == 3 && arrNumber[1] == '1') return sot(arrNumber[2]) + ' ' + des(arrNumber[1])
+
+    else return sot(arrNumber[2]) + ' ' + des(arrNumber[1]) + ' ' + ed(arrNumber[0]);
+    
+
 }
